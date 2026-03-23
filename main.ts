40161e8b96cbc59444ea01c5469e31a837d5b838
kovildiko <kovildiko8@gmail.com>
2026-03-23 19:18:31 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    basic.showNumber(3)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+    cuteBot.moveTime(cuteBot.Direction.forward, 50, 1)
+    basic.pause(5000)
+})
